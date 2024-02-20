649b66884f4f1c1418129d396351197d48615653
creativehimel <himel.phy@bsmrstu.edu.bd>
2024-02-21 00:15:42 +0600

    Language level crud done

--- app/Http/Controllers/Admin/Job/LanguageLevelController.php
+++ app/Http/Controllers/Admin/Job/LanguageLevelController.php
@@ -3,11 +3,40 @@
 namespace App\Http\Controllers\Admin\Job;
 
 use App\Http\Controllers\Controller;
+use App\Models\LanguageLevel;
 use Illuminate\Http\Request;
 
 class LanguageLevelController extends Controller
 {
     public function index(){
-        return view("");
+        $languageLevels = LanguageLevel::orderBy("name", "asc")->get();
+        return view("admin.pages.job.languageLevel", compact("languageLevels"));
+    }
+    public function store(Request $request){
+        $request->validate([
+            "name"=> "required|string|unique:language_levels,name",
+        ]);
+        LanguageLevel::create([
+            "name"=> $request->name,
+        ]);
+        notify()->success("Language levels created successfully.");
+        return redirect()->back();
+    }
+    public function update(Request $request, $id){
+        $languageLevel = LanguageLevel::findOrfail($id);
+        $request->validate([
+            "name"=> "required|string|unique:language_levels,name,".$languageLevel->id,
+        ]);
+        $languageLevel->update([
+            "name"=> $request->name,
+            "status" => $request->status
+        ]);
+        notify()->success("Language levels updated successfully.");
+        return redirect()->back();
+    }
+    public function destroy($id){
+        LanguageLevel::where("id", $id)->delete();
+        notify()->success("Language levels deleted successfully.");
+        return redirect()->back();
     }
 }
